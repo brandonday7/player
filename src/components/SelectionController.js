722f9610373cacaf23324952b66aa427bc9b3087
Brandon Day <brandontday7@gmail.com>
2019-02-13 18:34:38 -0500

    implemented a library for the time slider, rendering works, now to add the funcitonality

--- src/components/SelectionController.js
+++ src/components/SelectionController.js
@@ -1,5 +1,7 @@
 import React from "react"
 import PlayController from "./PlayController"
+import SlideRender from "./SlideRender"
+import { Slider, Direction } from 'react-player-controls'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faForward } from '@fortawesome/free-solid-svg-icons'
 import { faBackward } from '@fortawesome/free-solid-svg-icons'
@@ -8,9 +10,20 @@ import "./controller.css"
 
 const SelectionController = ({ nextTrack, prevTrack, pausePlay, playing}) => (
 	<div className="controller-container">
-		<FontAwesomeIcon className="icon change-song" onClick={prevTrack} icon={faBackward}/>
-		<PlayController onClick={pausePlay} playing={playing}/>
-		<FontAwesomeIcon className="icon change-song" onClick={nextTrack} icon={faForward}/>
+		<div className="buttons-container">
+			<FontAwesomeIcon className="icon change-song" onClick={prevTrack} icon={faBackward}/>
+			<PlayController onClick={pausePlay} playing={playing}/>
+			<FontAwesomeIcon className="icon change-song" onClick={nextTrack} icon={faForward}/>
+		</div>
+		<Slider
+		  direction={Direction.HORIZONTAL}
+		  isEnabled
+		  onChange={newValue => console.log(`clicked at ${newValue}`)}
+		  onChangeStart={startValue => console.log(`started dragging at ${startValue}`)}
+		  onChangeEnd={endValue => console.log(`stopped dragging at ${endValue}`)}
+  >
+  	<SlideRender />
+		</Slider>
 	</div>
 )
 
---
+++ src/components/SlideRender.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./controller.css"
+
+const SlideRender = ({}) => (
+	<div className="slide-render-container">
+	<div className="slide-progress"></div>
+	</div>
+)
+
+export default SlideRender
