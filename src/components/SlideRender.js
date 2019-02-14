f5a8da96659373c9f1d6dda33564999b925d678d
Brandon Day <brandontday7@gmail.com>
2019-02-13 19:20:36 -0500

    installed moment for time counter

--- src/components/SlideRender.js
+++ src/components/SlideRender.js
@@ -1,10 +1,11 @@
 import React from "react"
 import "./controller.css"
+import moment from "moment"
 
 const SlideRender = ({currentTime}) => (
-	<div>
-		<p>{currentTime}</p>
-		<div className="slide-render-container">
+	<div className="slide-render-container">
+		<p className>{moment(currentTime).format("m:ss")}</p>
+		<div className="slide-total">
 			<div className="slide-progress"></div>
 		</div>
 	</div>
