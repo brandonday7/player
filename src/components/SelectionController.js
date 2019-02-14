963a4eeec9e5a84b0159ebd5e1efe7bbf214c644
Brandon Day <brandontday7@gmail.com>
2019-02-13 19:43:32 -0500

    got slider displaying the current time as a width of the actual slider

--- src/components/SelectionController.js
+++ src/components/SelectionController.js
@@ -8,6 +8,12 @@ import { faBackward } from '@fortawesome/free-solid-svg-icons'
 
 import "./controller.css"
 
+const convertTime = time => {
+	const mins = Math.floor(time/60)
+	const secs = `${time % 60}`.padStart(2, '0')
+	return `${mins}:${secs}`
+}
+
 const SelectionController = ({ nextTrack, prevTrack, pausePlay, playing, seek, currentTime, duration}) => (
 	<div className="controller-container">
 		<div className="buttons-container">
@@ -15,15 +21,20 @@ const SelectionController = ({ nextTrack, prevTrack, pausePlay, playing, seek, c
 			<PlayController onClick={pausePlay} playing={playing}/>
 			<FontAwesomeIcon className="icon change-song" onClick={nextTrack} icon={faForward}/>
 		</div>
-		<Slider
-		  direction={Direction.HORIZONTAL}
-		  isEnabled
-		  onChange={newValue => seek(newValue)}
-		  onChangeStart={pausePlay}
-		  onChangeEnd={pausePlay}
-  >
-  	<SlideRender currentTime={currentTime} duration={duration}/>
-		</Slider>
+		
+		<div className="slide-render-container">
+			<p className="time-text">{convertTime(currentTime)}</p>
+			<Slider
+		  	direction={Direction.HORIZONTAL}
+		  	isEnabled
+		  	onChange={newValue => seek(newValue)}
+		  	onChangeStart={pausePlay}
+		  	onChangeEnd={pausePlay}
+  		>
+  			<SlideRender currentTime={currentTime} duration={duration}/>
+			</Slider>
+			<p className="time-text">{convertTime(duration - currentTime)}</p>
+		</div>
 	</div>
 )
 
--- src/components/SlideRender.js
+++ src/components/SlideRender.js
@@ -1,19 +1,9 @@
 import React from "react"
 import "./controller.css"
 
-const convertTime = time => {
-	const mins = Math.floor(time/60)
-	const secs = `${time % 60}`.padStart(2, '0')
-	return `${mins}:${secs}`
-}
-
-const SlideRender = ({ currentTime, duration }) => (
-	<div className="slide-render-container">
-		<p className="time-text">{convertTime(currentTime)}</p>
-		<div className="slide-total">
-			<div className="slide-progress"></div>
-		</div>
-		<p className="time-text">{convertTime(duration - currentTime)}</p>
+const SlideRender = ({currentTime, duration}) => (
+	<div className="slide-total">
+		<div style={{width: `${20*currentTime/duration}em`}} className="slide-progress"></div>
 	</div>
 )
 
